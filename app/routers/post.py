de98dea1608c0db66a224bb14118027567d49e79
abinaya15meenatchisundaram <abinaya.meenatchisundharam@gmail.com>
2025-08-26 22:47:58 +0530

    Skip query parameter to get post

--- app/routers/post.py
+++ app/routers/post.py
@@ -10,8 +10,10 @@ router = APIRouter(
 )
 
 @router.get("/",response_model =List[schemas.Post])
-def get_post(db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user), limit =10):
-    posts = db.query(models.Post).limit(limit).all()
+def get_post(db: Session = Depends(get_db),current_user: int = Depends(oauth2.get_current_user), 
+             limit: int =10, skip: int =0):
+    
+    posts = db.query(models.Post).limit(limit).offset(skip).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model =schemas.Post)
